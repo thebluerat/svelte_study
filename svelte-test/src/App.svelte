f59fccc2aa3404aeb77e87524d3cd91f55391089
thebluerat <riato101@naver.com>
2022-12-12 23:31:33 +0900

    svelte 공부 폴더 올림

---
+++ svelte-test/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Something from './Something.svelte'
+    let toggle = false
+</script>
+
+<button on:click={() => {toggle = !toggle}}>
+    Toggle
+</button>
+
+{#if toggle}
+    <Something />
+{/if}
