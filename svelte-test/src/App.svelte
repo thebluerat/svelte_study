eb027586313e2dd9b69b1209b8bfa408e98faf4c
thebluerat <riato101@naver.com>
2022-12-14 14:52:27 +0900

    lifecycle: tick

--- svelte-test/src/App.svelte
+++ svelte-test/src/App.svelte
@@ -1,14 +1,17 @@
-<script>
-    import {lifecycle, delayRender} from './lifecycle.js'
-    import Something from './Something.svelte'
+<!-- tick: 라이프사이클. 데이터가 갱신되고 나서 화면이 바뀌는 반응성을 가질 때까지 잠시 기다리게 해주는 능력이다. 실행 위치가 자유롭다. -->
 
-    let done = delayRender()
-    lifecycle()
+<script>
+    import { tick } from 'svelte'
+    let name= 'world'
+
+    async function handler() {
+        name = 'ttoobi'
+        await tick()
+        const h1 = document.querySelector('h1')
+        console.log(h1.innerText)
+    }
+    // tick을 쓰기 전: Svelte의 최적화 방법으로 내부의 로직을 전부 다 실행하고 나서 화면을 갱신한다. 따라서 클릭을 한 후에도 log에는 Hello world!가 뜬다.
+    // tick이라는 함수는 promise 객체를 반환한다. 따라서 await를 붙여서 비동기로 실행해줘야 하고, 그러면 함수 앞에 async를 붙여줘야 한다.
 </script>
 
-{#if $done}
-    <h1>Hello Lifecycle!</h1>
-{/if}
-
-<Something /> 
-<!-- Something을 별도의 컴포넌트로 선언함 -->
+<h1 on:click={handler}>Hello {name}!</h1>
--- svelte-test/src/Something.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import {delayRender} from './lifecycle.js'
-
-    let done = delayRender(1000) // 1초 뒤에 렌더링
-</script>
-
-{#if $done}
-    <h1>Something...</h1>
-{/if}
